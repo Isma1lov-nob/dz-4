36b231a0d7e96bb1ad7b51a4398d4f062e06ee16
Isma1lov-nob <115091585+Isma1lov-nob@users.noreply.github.com>
2023-01-17 21:31:28 +0500

    hw-1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import React from "react";
 
-import "./app.css"
-import MainPage from "./pages/mainPage/MainPage"
+class Timer extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {seconds: 0}
+  }
+tick(){
+  this.setState(state =>({seconds: state.seconds + 1}))
+}
 
+componentDidMount(){
+  this.interval = setInterval(()=> this.tick(), 1000)
 
+ }
+ componentWillUnmount(){
+  clearInterval(this.interval)
+ }
 
-function App (){
- return(
-  <div>
-    <MainPage/>
-  </div>
- )
+ render(){
+  return (
+    <div>
+      seconds:{this.state.seconds}
+    </div> 
+  )
+ }
 
 }
-export default App
+
+export default Timer
--- src/hooks/useFillter.jsx
+++
@@ -1,8 +0,0 @@
-export default function useFillter(search , array){
-   if(search){
-    return array.filter(el=> el.name.toLowerCase().includes(search.toLowerCase()))
-   }else{
-    return array
-   }
-
-}
